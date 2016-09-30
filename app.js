ec93ff51a89172041cbc7b8e0ac25b0ad7ee5fa2
rafaelkallis <rk@rafaelkallis.com>
2016-10-01 01:38:49 +0200

    9to5 push

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require('express');
 var http = require('http');
 var zlib = require('zlib');
 var Pool = require('pg').Pool;
+var Transaction = require('pg-transaction');
 var app = express();
 var schedule = require('node-schedule');
 
@@ -14,7 +15,7 @@ Object.values = Object.values || ((obj) => Object.keys(obj).map(key => obj[key])
 
 
 app.get('/', (client_req, client_res) => {
-  
+
 });
 
 var server_req = http.request({
@@ -43,35 +44,39 @@ server_req.end();
 // });
 
 var pool = new Pool({
-    user: process.env.POSTGRES_USERNAME,
-    database: process.env.POSTGRES_DATABASE,
-    password: process.env.POSTGRES_PASSWORD,
-    host: process.env.POSTGRES_HOST,
-    post: process.env.POSTGRES_PORT,
-    max: 10,
+    user: process.env.POSTGRES_USERNAME || 'postgres',
+    database: process.env.POSTGRES_DATABASE || 'postgres',
+    password: process.env.POSTGRES_PASSWORD || 'postgres',
+    host: process.env.POSTGRES_HOST || 'localhost',
+    post: process.env.POSTGRES_PORT || 5432,
+    max: 50,
     idleTimeoutMillis: 30000
 });
 
-// pool.connect(function (err, client, done) {
-//     !err && client.query(`
-//     CREATE TABLE IF NOT EXISTS "repository" (
-//         name VARCHAR(64) PRIMARY KEY,
-//         n_commits INT NOT NULL
-//     );
-//     CREATE TABLE IF NOT EXISTS "user" (
-//         hashed_email CHAR(40) PRIMARY KEY,
-//         any_commit_sha CHAR(40) NOT NULL
-//     );
-//     CREATE TABLE IF NOT EXISTS "commits" (
-//         repository_name VARCHAR(64) PRIMARY KEY,
-//         user_hashed_email CHAR(40) NOT NULL, n_commits INT NOT NULL,
-//         CONSTRAINT commits_repository_name_fk FOREIGN KEY (repository_name) REFERENCES repository (name),
-//         CONSTRAINT commits_user_hashed_email_fk FOREIGN KEY (user_hashed_email) REFERENCES "user" (hashed_email)
-//     );`, (err, result) => {
-//         err && console.error('error creating tables', err);
-//         done();
-//     }) || console.error('error getting client from pool', err);
-// });
+pool.connect(function (err, client, done) {
+    !err && client.query(`
+    CREATE TABLE IF NOT EXISTS "repository" (
+        name CHARACTER VARYING(100) PRIMARY KEY NOT NULL,
+        n_commits INTEGER NOT NULL
+    ); 
+    CREATE TABLE IF NOT EXISTS "user" (
+        hashed_email CHARACTER(40) PRIMARY KEY NOT NULL,
+        any_commit_sha CHARACTER(40) NOT NULL
+    );
+    CREATE TABLE IF NOT EXISTS "commits" (
+        repository_name CHARACTER VARYING(100) NOT NULL,
+        user_hashed_email CHARACTER(40) NOT NULL,
+        n_commits INTEGER NOT NULL,
+        PRIMARY KEY (repository_name, user_hashed_email),
+        FOREIGN KEY (repository_name) REFERENCES public.repository (name)
+        MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
+        FOREIGN KEY (user_hashed_email) REFERENCES public."user" (hashed_email)
+        MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
+    );`, (err) => {
+        err && console.error('error creating tables', err);
+        done();
+    }) || console.error('error getting client from pool', err);
+});
 
 function onRequestEnd(chunks) {
     console.log("Computation Started");
@@ -94,35 +99,17 @@ function onRequestEnd(chunks) {
     let user_query_args = genInsertStatements(author_commits.map((author_commit) => [author_commit.user_email_sha, author_commit.any_commit_sha]), 'user');
     let commits_query_args = genInsertStatements(author_commits.map((author_commit) => [author_commit.repo, author_commit.user_email_sha, author_commit.n_commits]), 'commits');
 
-    let begin_query_args = {
-        queries: ['BEGIN'],
-        args: []
-    };
-    let rollback_query_args = (err) => {
-        return {
-            queries: [err ? 'ROLLBACK' : 'COMMIT'],
-            args: []
+    pool.connect((err, client, done) => {
+            let tx = new Transaction(client);
+            tx.on('error', (err) => console.error(err));
+            tx.begin();
+            !err && tx.query('DELETE FROM "commits"; DELETE FROM "user"; DELETE FROM "repository"', (query_err) => err |= query_err);
+            !err && repository_query_args.queries.forEach((query, index) => tx.query(query, repository_query_args.args[index], (query_err) => err |= query_err));
+            !err && user_query_args.queries.forEach((query, index) => tx.query(query, user_query_args.args[index]));
+            !err && commits_query_args.queries.forEach((query, index) => tx.query(query, commits_query_args.args[index]));
+            !err && tx.commit() && console.log('Transaction Successful') && done() || tx.abort() && console.error("Transaction Aborted") && done(err);
         }
-    };
-    pool.connect(function (transaction_err, client, done) {
-        stackQueries(client, begin_query_args, transaction_err, (err) =>
-            stackQueries(client, repository_query_args, err, (err) =>
-                stackQueries(client, user_query_args, err, (err) =>
-                    stackQueries(client, commits_query_args, err, (err) =>
-                        stackQueries(client, rollback_query_args(err), err, (err) => {
-                            if(err){
-                                done(err);
-                                console.error('Error on transaction');
-                            } else {
-                                done();
-                                console.log('Transaction successful');
-                            }
-                        })
-                    )
-                )
-            )
-        );
-    });
+    );
 }
 
 function stringifyChunks(chunks) {
@@ -205,7 +192,7 @@ function reduceCommits(repository_commits) {
         });
 }
 
-const chunk_size = 5000;
+const chunk_size = 10000;
 
 function genInsertStatements(tuples, table_name) {
     let queries = [];
@@ -251,15 +238,15 @@ function genPlaceholder(size, index) {
     };
 }
 
-function stackQueries(client, queries_args, transaction_err, callback) {
-    if (!transaction_err && queries_args.queries.length) {
-        let query = queries_args.queries.pop();
-        let args = queries_args.args.pop();
-        console.log(query);
-        client.query(query, args, (err) => stackQueries(client, queries_args, transaction_err |= err, callback));
-    } else {
-        callback(transaction_err);
-    }
-}
+// function stackQueries(client, queries_args, transaction_err, callback) {
+//     if (!transaction_err && queries_args.queries.length) {
+//         let query = queries_args.queries.pop();
+//         let args = queries_args.args.pop();
+//         console.log(query);
+//         client.query(query, args, (err) => stackQueries(client, queries_args, transaction_err |= err, callback));
+//     } else {
+//         callback(transaction_err);
+//     }
+// }
 
 app.listen(8080);
